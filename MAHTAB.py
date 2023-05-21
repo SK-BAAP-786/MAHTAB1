fb671510c038dc63ba0a02f8edcb0b7305caed7c
SK BAAP G <111557947+SK-BAAP-786@users.noreply.github.com>
2023-05-21 21:49:00 +0530

    Create MAHTAB.py

---
+++ MAHTAB.py
@@ -0,0 +1,35 @@
+import os,platform
+
+ 
+
+os.system('git pull')
+
+ 
+
+ 
+
+ 
+
+SK=platform.architecture()[0]
+
+ 
+
+if SK=="32bit":
+
+ 
+
+    print('Sorry 32 Bit Not Supported...')
+
+ 
+
+elif SK=="64bit":
+
+ 
+
+    
+
+ 
+
+    __import__("MAHTAB22_enc")
+
+ 
